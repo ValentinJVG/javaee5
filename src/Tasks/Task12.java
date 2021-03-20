63d8f42867a1354a1d4bf915449728c9a745357b
Valentin Jevsejev <valentin.jevsejev@gmail.com>
2021-03-20 19:45:48 +0200

    added some files

--- src/Tasks/Task12.java
+++ src/Tasks/Task12.java
@@ -8,10 +8,10 @@ public class Task12 {
         System.out.println("Enter text: ");
         String string = scanner.nextLine();
         int spaces = (string.length()) - string.replaceAll(" ", "").length();
-        float percentInString = (float)(spaces * 100f) / string.length() ;
+        float percentInString = (spaces * 100f) / string.length() ;
         System.out.println("String length is " + string.length());
         System.out.println("Amount of spaces is " + spaces);
-        System.out.println("Per cent of spaces" + percentInString + "%");
+        System.out.println("Per cent of spaces " + percentInString + "%");
 
     }
 }
