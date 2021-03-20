1a4d13d29a3a51e321819b335d16ea2a125bc5de
Valentin Jevsejev <valentin.jevsejev@gmail.com>
2021-03-20 21:10:51 +0200

    added some files

--- src/Tasks/Task15_ToDo.java
+++ src/Tasks/Task15_ToDo.java
@@ -1,28 +1,21 @@
-//package Tasks;
-//
-//import java.util.Scanner;
-//import java.util.Arrays;
-//
-//public class Task15_ToDo {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int [] numbers = new int[10];
-//        for (int i = 0; i < numbers.length; i++)
-//        {
-//            numbers[i] = scanner.nextInt()
-//        }
-//        for (int j = 0; j < numbers.length; j++) {
-//            if (numbers[i] == numbers[j])[{
-//                System.out.println();
-//            }
-//        }
-//        System.out.println(numbers);
-//
-//
-//
-//
-//
-//
-//        }
-//    }
-//
+package Tasks;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Task15_ToDo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter 10 numbers: ");
+
+        int[] numbers = new int[10];
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = scanner.nextInt();
+                }
+            System.out.println(Arrays.toString(numbers));
+
+
+        }
+    }
+
+
--- src/Tasks/Task18_ToDo.java
+++ src/Tasks/Task18_ToDo.java
@@ -6,6 +6,6 @@
 //    public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
 //
-//        while ()
-//    }
+//
+//
 //}
